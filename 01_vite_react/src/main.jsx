a99f5821de4b09c63eeee929e3c16fc7e41d433e
Pratik-Shrivastava <pratikkumar441@gmail.com>
2024-01-21 11:35:53 +0530

    learnt why we cannot use our custom created element in App

--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -3,7 +3,33 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Pratik from './Pratik.jsx'
 
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom App !</h1>        
+        </div>
+    )
+}
+const ReactElement = {
+    // But we have written this reactElement according to us.
 
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit Google'
+}
+
+const anotherElement = (
+    <a href='https://google.com' target='_blank'>Visit Google</a>
+);
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <App/>
+    // Since it is a function then we can also write it as
+    // MyApp()
+    // <ReactElement/>  //We cannot write it as.
+    // anotherElement  //It works fine
+
+    
 )
